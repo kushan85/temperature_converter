4a80c4e5b4485f9d11d42f95bd8e1bc79896481c
Kushan Shah <kshah@zynga.com>
2014-01-20 10:11:51 -0800

    Initial Commit

---
+++ TemperatureConverter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TemperatureConverter
+//
+//  Created by Kushan Shah on 1/20/14.
+//  Copyright (c) 2014 Kushan Shah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
